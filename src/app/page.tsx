53d191727cdd582e90cef36536326facd061de39
SeharRehan <163435415+SeharRehan@users.noreply.github.com>
2024-12-07 18:56:25 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,35 @@
+"use client";
+import Contact from "@/app/components/Contact";
+import Hero from "@/app/components/Hero";
+import Projects from "@/app/components/Projects";
+import Skills from "@/app/components/Skills";
+import About from "@/app/components/About";
+
+
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
+
+export default function Home() {
+  useEffect(() => {
+    AOS.init({
+      easing: "ease-out-back",
+      duration: 1200,
+      delay: 100,
+      mirror: true,
+      anchorPlacement: "bottom-bottom",
+      offset: 160,
+    });
+    AOS.refresh();
+  }, []);
+
+  return (
+    <main>
+      <Hero />
+      <Projects />
+      <Skills />
+      <Contact />
+      <About />
+    </main>
+  );
+}
